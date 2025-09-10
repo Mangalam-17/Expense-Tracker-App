263ab0adfea51ae3ec8ad8245b917f1fd1e3849b
Mangalam Mishra <mangalamab17@gmail.com>
2025-09-10 22:10:32 +0530

    setup MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("Failed to connect MongoDB : ", err));
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello There");
+  res.send("API is running successfully !");
 });
 
 app.listen(PORT, () => {
