12b1cc8aa1c91fd3b98249178b3af1657c08867e
Mangalam Mishra <mangalamab17@gmail.com>
2025-09-11 16:24:55 +0530

    add create transaction route

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -2,4 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Transaction = require("../models/Trasaction");
 
+// CRUD Operation
+// Create Operation, creating a transaction
+router.post("/", async (req, res) => {
+  try {
+    const newTransaction = new Transaction(req.body);
+    const savedTransaction = await newTransaction.save();
+    res.status(201).json(savedTransaction);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 module.exports = router;
