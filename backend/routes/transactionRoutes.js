bb3834fdc62cadbb63c6a6ff941445f3209f2d82
Mangalam Mishra <mangalamab17@gmail.com>
2025-09-11 16:27:10 +0530

    add read all & single transaction

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -14,4 +14,26 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Read Operation, reading all transactions
+router.get("/", async (req, res) => {
+  try {
+    const transactions = await Transaction.find();
+    res.json(transactions);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Read Operation, reading a single transaction via id
+router.get("/:id", async (req, res) => {
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+    if (!transaction)
+      return res.status(404).json({ error: "Transaction not found" });
+    res.json(transaction);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;
